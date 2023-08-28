da2b6bda3301b9c0228e91f845bf02ba0b2302cb
hmigl <88746406+hmigl@users.noreply.github.com>
2023-08-28 16:17:29 -0300

    gottem go horse

--- aluga-facil/src/main/java/com/alugafacil/AlugaFacilApplication.java
+++ aluga-facil/src/main/java/com/alugafacil/AlugaFacilApplication.java
@@ -1,5 +1,21 @@
 package com.alugafacil;
 
+import com.alugafacil.imovel.Imovel;
+import com.alugafacil.proprietario.Proprietario;
+
+import java.math.BigDecimal;
+
 public class AlugaFacilApplication {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Proprietario proprietario = new Proprietario("felix", "4691456156");
+
+        proprietario.adicionaImovel(
+                new Imovel(proprietario, "cajacity", BigDecimal.valueOf(420), 5, 8));
+
+        proprietario.adicionaImovel(
+                new Imovel(proprietario, "ondina", BigDecimal.valueOf(69420), 2, 6));
+
+        System.out.println(proprietario);
+        System.out.println(proprietario.quantidadeDeImoveis());
+    }
 }
