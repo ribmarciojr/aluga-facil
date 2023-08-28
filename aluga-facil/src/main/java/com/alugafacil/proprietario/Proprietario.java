289779356256e6eaf46823c385cec8207505a3ba
hmigl <88746406+hmigl@users.noreply.github.com>
2023-08-28 15:30:52 -0300

    feat: add 'Proprietario'

---
+++ aluga-facil/src/main/java/com/alugafacil/proprietario/Proprietario.java
@@ -0,0 +1,47 @@
+package com.alugafacil.proprietario;
+
+import com.alugafacil.imovel.Imovel;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Proprietario {
+    private final String nome;
+    private final String cpf;
+    private Set<Imovel> imoveis = new HashSet<>();
+
+    public Proprietario(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public boolean adicionaImovel(Imovel imovel) {
+        Objects.requireNonNull(imovel, "Você precisa adicionar um imóvel válido");
+        return this.imoveis.add(imovel);
+    }
+
+    public boolean removeImovel(Imovel imovel) {
+        Objects.requireNonNull(imovel, "Você precisa remover um imóvel válido");
+        /*
+         * Não se pode remover um imóvel que não esteja assoaciado a este proprietário
+         * */
+        if (!imovel.getProprietario().equals(this)) {
+            throw new IllegalArgumentException("...");
+        }
+        return this.imoveis.remove(imovel);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Proprietario that = (Proprietario) o;
+        return Objects.equals(nome, that.nome) && Objects.equals(cpf, that.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cpf);
+    }
+}
